aab2890c0fa9b9ca6e1ff6483f2b9b6cf75493cb
LowLuck <cfif905@bk.ru>
2021-05-27 11:37:58 +0300

    Added indexing?

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 
 def main(superlist, stage):
-    global path
+    global path, pathlist
     for j in superlist:
         if os.path.isdir(path + f'\\{j}'):
             print(' ' * (stage * 4) + j)
@@ -10,8 +10,11 @@ def main(superlist, stage):
             main(os.listdir(path), stage + 1)
         else:
             print(' ' * (stage * 4) + j)
+            pathlist[j] = path + f'\\{j}'
 
 
+pathlist = dict()
 path = 'E:\\Testfolder'
 superlist = os.listdir(path)
 main(superlist, 0)
+print(pathlist)
