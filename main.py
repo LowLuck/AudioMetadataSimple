72c5ce8e3db19e91be48cd74004ca4f6e08055a5
LowLuck <cfif905@bk.ru>
2021-06-08 14:15:14 +0300

    Now this works with audiofiles

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import eyed3
-from pydub import AudioSegment
 
 
 def main(superlist, stage):
@@ -21,3 +20,28 @@ path = 'E:\\Testfolder'
 superlist = os.listdir(path)
 main(superlist, 0)
 print(pathlist)
+
+while True:
+    w = input('\n Please, type name of audio file that you want to get (not all audiotypes supported) \n')
+    if w in pathlist:
+        mode = input('Type R to reading mode, or W to writing mode \n')
+        if mode == 'R':
+            audiofile = eyed3.load(pathlist[w])
+            print(f'Artist: {audiofile.tag.artist}')
+            print(f'Album: {audiofile.tag.album}')
+            print(f'Title: {audiofile.tag.title}')
+            print(f'Track_num: {audiofile.tag.track_num}')
+            break
+        elif mode == 'W':
+            audiofile = eyed3.load(pathlist[w])
+            audiofile.tag.artist = input('type artist tag \n')
+            audiofile.tag.album = input('type album tag \n')
+            audiofile.tag.title = input('type title tag \n')
+            audiofile.tag.track_num = input('type track_num tag \n')
+            audiofile.tag.save()
+            break
+        else:
+            print('Err')
+    else:
+        print('File not found')
+
